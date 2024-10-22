0e37c6e8dfc895b61b8b2839903a030c6933698d
sayyss <sawaisayam@gmail.com>
2024-10-22 15:25:13 -0500

    added type checking for utils

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
 # helper functions -> one hot encoding, any vector math
 
-def one_hot_encode(words, vocab):
-def prepare_data(words):
+def one_hot_encode(words: List[str], vocab: int) -> List[float]:
+
+def prepare_data(text_file: str) -> Tuple[List[str], int]:
+
+
+def prepare_input(words, vocab) -> Tuple[List[int], List[int]]:
 
