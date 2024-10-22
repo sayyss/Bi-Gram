d3c3178aef468a3c85abd1f013a14d283a358a2d
sayyss <sawaisayam@gmail.com>
2024-10-22 15:02:27 -0500

    more details

--- model.py
+++ model.py
@@ -4,11 +4,12 @@ class Bigram:
         self.vocab = vocab
         self.matrix = torch.randn(vocab, vocab)
 
-
     def __call__(self, x):
 
     def forward(self, x):
-
+        # x * matrix
+        # return logits(vector)
 
     def generate(self, context, length):
-
+        # given a word, get length amount of words from model and return
+        # returns a string
--- utils.py
+++ utils.py
@@ -1,1 +1,6 @@
 # helper functions -> one hot encoding, any vector math
+
+def one_hot_encode(words):
+
+def prepare_data(words):
+
