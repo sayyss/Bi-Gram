959e8d17ee5509a4cf325000b23d583bbf6e5066
sayyss <sawaisayam@gmail.com>
2024-10-27 19:49:59 -0500

    added clarification for one_hot

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # helper functions -> one hot encoding, any vector math
 
 def one_hot_encode(words: List[int], vocab: int) -> List[float]:
+    # uses pytorch's one_hot function to create one hot vectors of class size: vocab
 
 def prepare_data(text_file: str):
 
