87411082e999b9bd3e228b657f1fcb701b0a120a
sayyss <sawaisayam@gmail.com>
2024-11-15 14:48:01 -0600

    npl-helper

---
+++ NPL_helper.py
@@ -0,0 +1,26 @@
+
+### Takes in x, and y, uses CLE formula to total loss
+### returns total loss(a scalar tensor)
+### x: tensor([])
+### y: tensor([])
+def CLE(x,y):
+    return
+
+
+### X: tensor([])
+### returns a tensor same size x as softmax function applied
+def softmax(x):
+    return
+
+### returns one_hot encoded values 
+### x: tensor([])
+### n: number of classes to encode into
+def one_hot_encode(x, n):
+    return
+
+
+
+
+
+
+
